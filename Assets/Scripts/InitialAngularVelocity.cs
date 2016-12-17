6d553a07ce93d8b49e0f3aed96b653fa8ad1dcf1
Coenraad <coenraad.stijne@gmail.com>
2016-12-17 13:21:51 +0100

    billboard laptop name

---
+++ Assets/Scripts/InitialAngularVelocity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialAngularVelocity : MonoBehaviour {
+
+public Vector3 AngularVelocity;
+
+	// Use this for initialization
+	void Start () {
+		var rb = this.GetComponent<Rigidbody>();
+
+		if (rb != null)
+		{
+			rb.AddTorque(AngularVelocity, ForceMode.VelocityChange);
+		}
+	}
+}
