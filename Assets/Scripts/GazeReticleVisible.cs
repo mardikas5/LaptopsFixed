030c5d5f1ba1a8fd429ab5fb97bf2d95809d6753
Coenraad Stijne <coenraad.stijne@gmail.com>
2016-12-18 22:45:17 +0100

    only show reticle within range of gazable object

---
+++ Assets/Scripts/GazeReticleVisible.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeReticleVisible : MonoBehaviour {
+
+    private int count;
+    public GameObject Reticle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Reticle.SetActive(count != 0);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        count++;
+        Debug.Log("Gazable object entered range");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        count--;
+        Debug.Log("Gazable object left range");
+    }
+}
